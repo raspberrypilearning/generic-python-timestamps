039d8d31d5eceb9d49d59d5234074706c0bace7a
Marc Scott <marc@raspberrypi.org>
2017-07-04 14:27:42 +0100

    unpublish

--- en/resources/example.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-print('{:%Y-%m-%d-%H-%M-%S}'.format(datetime.now()))
-print('{:%Y-%b-%d-%H-%M-%S}'.format(datetime.now()))
-print('{:%H-%M-%S}'.format(datetime.now()))
